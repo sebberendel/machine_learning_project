9d560add56539c5c599cce07fe1a313f391d9b13
sebberendel <sebbe.rendel@gmail.com>
2026-02-17 14:35:49 +0100

    korv

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -44,6 +44,7 @@ X = df.drop('increase_stock', axis=1)
 X_holdout = X.sample(frac=0.2, random_state=1)  # 20% holdout set
 X = X.drop(X_holdout.index)  # Resterande 80% för träning
 y = df['increase_stock']
+y_holdout = y.loc[X_holdout.index]
 
 # --- Columns that should NOT be scaled ---
 day_cols = [c for c in X.columns if c.startswith("day_")]
